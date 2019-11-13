8614b66749ad592b4e7ae84c25e9845ab00f71e7
Code <code@COCTL-41286.local>
2019-11-13 15:36:12 -0800

    First Changes

--- script/script.js
+++ script/script.js
@@ -1,59 +1,83 @@
-let totalQuestions = 20;
-let diff = ""; // Can be medium or hard also
-let triviaQ = [];
+//---------Set and Get Variables----------//
 
-function LoadQuestions() {
+// let domBtn = document.getElementById('dom');
+// let randomBtn = document.getElementById('random');
+// let directoryBtn = document.getElementById('directory');
+// let triviaBtn = document.getElementById('trivia');
+
+//----------Add Event Listeners---------//
+// domBtn.addEventListener('click', function (e) {
+    //     console.log("clicked dom");
+    //     loadHTML('../inject/dom.html');
+    // });
+    
+    // randomBtn.addEventListener('click', function (e) {
+        //     loadHTML('../inject/random.html');
+        // });
+        
+        // directoryBtn.addEventListener('click', function (e) {
+            //     loadHTML('../inject/directory.html');
+            // });
+            
+            //----------injecting HTML Pages-----------//
+
+
+
+
+            
+let injectContent = document.getElementById('inject');
+let body = document.getElementById('body');
+  
+
+function injectHTML(url) {
+
     let xmlhttp = new XMLHttpRequest();
-    let url = "";
-
-    if (diff == "easy") {
-        url = "../data/ezQ.json";
-    }
-    else if (diff == "medium") {
-        url = "../data/mQ.json";
-    }
-    else if (diff == "hard") {
-        url = "../data/hQ.json";
-    }
-    // else{
-    //     url = "../data/ezQ.json";
-    // }
 
     xmlhttp.onreadystatechange = function () {
+
         if (this.readyState == 4 && this.status == 200) {
-            let myArr = JSON.parse(this.responseText);
-            allQuestions(myArr);
+            let myArr = this.responseText;//JSON.parse(this.responseText);
+            
+            if(url === '../inject/title.html'){
+                loadTitle(myArr);
+            }
+            else if(url === '../inject/menu.html'){
+                loadMenu(myArr);
+            }
+            else if(url === '../inject/game.html'){
+                loadGame(myArr);
+            }
+            else{
+                console.log('Check your if statement in InjectHTML');
+            }
         }
+
     };
+    //opens connection
     xmlhttp.open("GET", url, true);
+
+    //pulls the request
     xmlhttp.send();
-}
-
-function allQuestions(q) {
-    console.log(q.ezQ[26]);
-    let qNum = 0;
-    for (let i = 0; i < totalQuestions; i++) {
-        qNum = Math.floor(Math.random() * q.ezQ.length);
-        //console.log(qNum);
-        triviaQ.push(q.ezQ[qNum]);
-        q.ezQ.splice(qNum, 1);
-
-    }
-    console.log(triviaQ);
-
 
 }
 
-document.getElementById("easy-diff").addEventListener('click', function(){
-    diff = "easy";
-});
+function loadTitle(info){
+    injectContent.innerHTML = info;
+    body.className = 'title-bg';
+    //get stuff
+    //add event listeners
+    let titleToMenuBtn = document.getElementById('titleToMenuBtn').addEventListener('click', function(e){
+        injectHTML('../inject/menu.html');
+    });
+}
 
-document.getElementById("med-diff").addEventListener('click', function(){
-    diff = "medium";
-});
+function loadMenu(info){
+    injectContent.innerHTML = info;
+}
 
-document.getElementById("hard-diff").addEventListener('click', function(){
-    diff = "hard";
-});
+function loadGame(info){
+    injectContent.innerHTML = info;
+}
 
-LoadQuestions(diff);
+
+injectHTML('../inject/title.html');
